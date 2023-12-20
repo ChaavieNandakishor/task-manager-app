01c9471d63f76d513ff28dfee21a906592329911
ChaavieNandakishor <chaavienandakishor@gmail.com>
2023-12-20 15:14:13 +0530

    added:sign in page

--- App.js
+++ App.js
@@ -1,62 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  Button,
-  Image,
-  Pressable,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-} from "react-native";
-import welcomeImage from "./assets/images/welcome.png";
+import Welcome from "./src/components/screens/Welcome";
+import Signin from "./src/components/screens/Signin";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={{ paddingTop: 100, alignItems: "center" }}>
-        <Text style={{ fontSize: 25 }}>Your tasks, your plan</Text>
-      </View>
-      <View style={{ alignItems: "center" }}>
-        <Image source={welcomeImage} style={{ height: 250 }} />
-      </View>
-      <View
-        style={{
-          justifyContent: "space-around",
-          gap: 20,
-          display: "flex",
-          alignItems: "center",
-          paddingBottom: 90,
-        }}
-      >
-        <Pressable
-          style={{
-            width: "80%",
-            backgroundColor: "red",
-            borderRadius: 10,
-            overflow: "hidden",
-          }}
-        >
-          <Button color="#CD5C5C" title="Sign up" />
-        </Pressable>
-        <View style={{ display: "flex", flexDirection: "row" }}>
-          <Text>Dont have an account? </Text>
-          <Text style={{ color: "#CD5C5C" }}>Signup</Text>
-        </View>
-      </View>
-    </View>
+    <>
+    <StatusBar style="auto" />
+      <Signin />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "space-between",
-  },
-  redBorder: {
-    borderWidth: 2,
-    borderColor: "blue",
-    borderRadius: 10,
-  },
-});
---
+++ src/components/screens/Signin.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+export default function Signin() {
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Text style={{ fontSize: 30, paddingLeft: 30, paddingTop: 100 }}>
+            Sign In
+          </Text>
+          <Text style={{ fontSize: 14, paddingLeft: 30 }}>
+            Sign in to your account
+          </Text>
+        </View>
+        <View style={styles.textInputContainer}>
+          <TextInput style={styles.textInput} />
+          <TextInput style={styles.textInput} />
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Pressable style={styles.pressable}>
+            <Button color="#CD5C5C" title="Sign up" />
+          </Pressable>
+        </View>
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
+          <View
+            style={{
+              marginTop: 150,
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            <Text>Dont have an account? </Text>
+            <Text style={{ color: "#CD5C5C" }}>Signup</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  redBorder: {
+    borderWidth: 2,
+    borderColor: "blue",
+    borderRadius: 10,
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: "#CD5C5C",
+    borderRadius: 10,
+    height: 47,
+    width: "85%",
+  },
+  pressable: {
+    width: "85%",
+    backgroundColor: "red",
+    borderRadius: 10,
+    marginTop: 50,
+    overflow: "hidden",
+  },
+  textInputContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    display: "flex",
+    gap: 35,
+  },
+});
